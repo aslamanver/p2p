e48d89d2567f58e3ce79483b47a762c645e37315
Aslam Anver <slam.anver9@gmail.com>
2021-04-01 01:54:30 +0530

    test

--- p2psdk/src/androidTest/java/com/aslam/p2p/ExampleInstrumentedTest.java
+++ p2psdk/src/androidTest/java/com/aslam/p2p/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.payable.p2p.test", appContext.getPackageName());
+        assertEquals("com.aslam.p2p.test", appContext.getPackageName());
     }
 }
