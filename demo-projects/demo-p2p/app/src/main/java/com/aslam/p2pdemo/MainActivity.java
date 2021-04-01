7e1247d33d78e42cc95e31e9359c6b8ae325b50b
Aslam Anver <slam.anver9@gmail.com>
2021-04-01 14:56:41 +0530

    project - changes

--- demo-projects/demo-p2p/app/src/main/java/com/aslam/p2pdemo/MainActivity.java
+++ demo-projects/demo-p2p/app/src/main/java/com/aslam/p2pdemo/MainActivity.java
@@ -122,6 +122,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onStop() {
         super.onStop();
+        p2pService.setActivityListener(null);
         unbindService(mConnection);
     }
 
